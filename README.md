bf30736dc5bc65bc47a9534ab8ffebcdb599be2f
Garrett Moore <robertgarrettmoore@gmail.com>
2019-02-11 22:48:31 -0800

    first completed iteration of tumblin game

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # tumblin-game
+
+Made Feb 2019 by Garrett Moore - WDI student at General Assembly Seattle, WA.
+
+# CONTROLS
+
+Forward Arrow Key === Move player forward
+F Key === Fire projectile
+
+# ABOUT
+
+I created Tumblin... using HTML5 Canvas, Javascript, and CSS. 
+
+### 2/1 
+    I began wireframing and outlining the general concepts behind the game. Knowing that I was to be using the technologies listed above, I started to sketch out some basic functionality and how I might implement them.
+
+![alt text](/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/IMG_5429.JPG "Beginning sketch 1")
+![alt text](/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/IMG_5430.JPG "Beginning sketch 2")
+
+### 2/4 
+    Drew up basic canvas & background/player/enemy variables. Collected all images and music via OpenGameArt.org.
+
+### n2/5 
+    Initialized a moving background to stretch the length of each level. I used photoshop to repeat each background image to given length that would make up the width of each level (~ 630 px). Was able to draw on the individual characters at their respective levels throughout game, but at this point was only able to draw each one once. Next steps needed were to write functions to repeatedly draw each enemy a given number of times for each level. 
+
+### 2/6 
+    Created a handler for any enemy character to be passed into and repeatedly drawn from
+    example: 
+
+                                function Entity(x, y, width, height, src) {
+                                    this.x = x;
+                                    this.y = y;
+                                    this.width = width;
+                                    this.height = height;
+                                    this.src = src;
+                                };
+
+    Then I was able to set intervals at different times to push each enemy variable 
+    example:
+                 var scorpion = new Entity (player.x + 1000, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Scorpion.png');
+
+to an Enemies array, then subsequently draw them on the screen every couple of seconds. Since the X value was set based on the player's - it ensured that each enemy would only be drawn on in front of the player by a set amount of pixels.
+
+### 2/7 
+    Wrote in a shooting mechanic - similar to how I handled the enemy characters. I created a template that I could just pass the new bullet variable into, then have it set to be drawn on and propelled forward everytime a specific key event occured.
+
+### 2/8 
+    Programmed collision detection logic for whenever a player encountered an enemy, or an enemy encountered a player projectile. I used a basic rectangular collision alogorithm with some slightly tweaked parameters to have it trigger an event when either of these collisions occured. In the case of a player collision with the enemy, the enemy was bounced backward and the lives count was reduced by one. When an enemy collided with a player projectile, the image was changed to that of an explosion, and the player score was incremented by 10.
+
+### 2/9 
+    Set background music to be triggered at each level change, as well as different sounds for either type of collision. Unable to succesfully have the level one music play everytime the page is loaded. I've been experimenting with different preloading methods, and have yet to find one that works all of the time. 
+
+### 2/10 
+    Changed last level enemy to stop scrolling of the backround image. Planning to program more advanced kind of AI for this (ie movements based on player location, enemy projectiles, etc).
+
+### 2/11 
+    Refactoring code and trying to enable player character to roll as it's moved along the screen. Unsucessful in rolling character, but hoping to get it sorted.
+
+## Things I would like to add
+
+Upon revisting this project, it is my hope that I can add some functionality and design that I was unable to this first time around. Smooth transitions between levels, boss AI, rolling player chracter being the top priorities.  
--- js/app.js
+++ js/app.js
@@ -7,15 +7,24 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 var ctx = canvas.getContext('2d');
 
-//timer variables
+//audio variables
+var lvlOneMusic = new sound("/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/gone_fishin_by_memoraphile_CC0.mp3");
+var lvlTwoMusic = new sound("/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/beary_fishy_menu_screen_by_memoraphile_CC0.mp3");
+var lvlThreeMusic = new sound("/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/Farewell_To_Nova_Scotia_Popular_Folk_Song_Akkordica_Anglo-German_Concertina_VST_Plugin.mp3");
+var laser = new sound("/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/laser1.wav");
+var playerHit = new sound('/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/swish_3.wav');
+var gameOverSound = new sound('/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/GameOver.wav');
+var winMusic = new sound('/Users/garrettmoore/Code/Unit1/tumblin-game/sounds/win music 2.wav');
+
+// timer variables
 
 var timeHandler = null;
 var seconds = 3;
 var enemyInterval = null;
 var enemyIntervalTwo = null;
-var enemyIntervalThree = null;
+var enemyBulletInterval = null;
 
-//var to hold current background location
+// var to hold current background location
 var currentBackground;
 
 // dom score and life count variables 
@@ -23,12 +32,20 @@ var currentBackground;
 var headerText = document.getElementById('header');
 var scoreBox = document.getElementById('score-num');
 var lifeCount = document.getElementById('lives-num');
+var restart = document.getElementById('button');
 var lives = 3;
+var score = 0;
 
-//game states
+// game states
 var levelOne = true;
 var levelTwo = false;
 var levelThree = false;
+
+// player movement
+var minY = ((innerWidth/2) - 30);
+var maxY = ((innerWidth/2) + 30);
+var pace = 5;
+
 // template for enemy characters
 
 function Entity(x, y, width, height, src) {
@@ -47,90 +64,139 @@ Entity.prototype.draw = function() {
 
 // enemy characters
 var enemies = [];
-
+var bossCount = 0;
+var boss = [];
+var maxEnemies = 5;
 
 // set interval to create level one enemies
 
 enemyInterval = setInterval(function(){
-    var scorpion = new Entity (player.x + 600, (innerWidth/2) + 10, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Scorpion.png');
-    var cowboy = new Entity (player.x + 800, (innerWidth/2) - 5, 100, 100,'/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Cowboy4_walk with gun_3.png');
-    enemies.push(cowboy, scorpion);
-    enemies.shift();
-}, 4000);
-
+  var scorpion = new Entity (player.x + 1000, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Scorpion.png');
+  var cowboy = new Entity (player.x + 600, (innerWidth/2) - 10, 100, 100,'/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Cowboy4_walk with gun_3.png');
+  if (enemies.length < maxEnemies) {
+  enemies.push(cowboy, scorpion);
+  }  
+}, 5000);
 
 // clear enemy function
 var clearEnemies = function() {
-    for(let i = 0; i < enemies.length; i++) {
-        enemies.pop();
-        console.log(enemies);
-    }
+  for(let i = 0; i < enemies.length; i++) {
+    enemies.pop();
+  }
+}
+
+// create handler for audio files
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
 }
 
 // background image objects & template
 
-function newBackground(x, y, width, height, src) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.src = src;
+function changeBackground(x, y, width, height, src) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.src = src;
 }
 
-newBackground.prototype.draw = function() {
-    var backImg = new Image();
-    backImg.src = this.src;
-    ctx.drawImage(backImg, this.x, this.y, this.width, this.height);
+changeBackground.prototype.draw = function() {
+  var backImg = new Image();
+  backImg.src = this.src;
+  ctx.drawImage(backImg, this.x, this.y, this.width, this.height);
 };
 
-var bgOne = new newBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/desert_BG.png');
-var bgTwo = new newBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/swamp.png');
-var bgThree = new newBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/country field.png');
+var bgOne = new changeBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/desert_BG.png');
+var bgTwo = new changeBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/swamp.png');
+var bgThree = new changeBackground(0, 0, 3960, 620, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/country field.png');
 
 // draw on player character
 var playerWidth = 100;
 var playerHeight = 100;
 var playerImg = new Image();
-    playerImg.src = '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/tumbleweed2.png';
+  playerImg.src = '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw.png';
+
+var playerImgArray = ['/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw.png', '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw2.png',
+  '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw3.png', '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw4.png'];
 
 
 var player = {
   x: innerWidth/12 - (playerWidth/2),
   y: innerHeight - (playerHeight+80),
-  width: 100,
-  height: 100,
+  width: 250,
+  height: 250,
   draw: function() {
     if (this.x <= 0) {
-        this.x = 0;
+      this.x = 0;
   } else if (this.y >= (innerHeight - this.height)) {
       this.y = (innerHeight - this.height);
-    } 
+  } 
     ctx.drawImage(playerImg, this.x, this.y, this.width, this.height);
   }
 }
 
 // dom/key movement object & event listeners
 var map = {
-    37: false, // Left Arrow
-    39: false, // Right Arrow
-    83: false, // S key
+  39: false, // right arrow - move forward
+  70: false, // f key - shoot
+  83: false, // s key - jump
 }
 
-//level two enemies
+// shooting mechanics
+
+var bullets = [];
+var enemyBullets = [];
+
+function newBullet(x, y, width, height, src) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.src = src;
+}
+
+newBullet.prototype.draw = function (){
+  var bulletImg = new Image();
+  bulletImg.src = this.src;
+  ctx.drawImage(bulletImg, this.x, this.y, this.width, this.height);
+}
+
+// level two enemies
 var fox = new Entity (player.x + 500, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/002.png');
 
 function initGame() {
 document.addEventListener('keydown', function(event){
-  if (event.keyCode in map) {
-    map[event.keyCode] = true;
-      if (map[39]) {
-        currentBackground.x -= 5;
-        player.x += 1;
-        console.log(player.x);
-    } else if (map[37]) {
-        player.x -= 1;
-        }}
-    });
+if (event.keyCode in map) {
+  map[event.keyCode] = true;
+  if (map[39]) {
+    if (currentBackground === bgOne || currentBackground === bgTwo) {
+      currentBackground.x -= pace;
+    }
+    player.x += 1;
+    for(let i = 0; i < playerImgArray; i++) {
+      playerImg = new Image();
+      playerImg.src = playerImgArray[i];
+    }
+
+} else if (map[70]) {
+    var bullet = new newBullet(player.x + 75, player.y + 75, 100, 100,'/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/newtw.png');
+    bullets.push(bullet);
+    if (bullets.length >= 3) {
+      bullets.length = 0;
+    }
+  } 
+}});
 
 document.addEventListener('keyup', function(event){
   if(event.keyCode in map) {
@@ -140,21 +206,18 @@ document.addEventListener('keyup', function(event){
 };
 
 // set timer & lock keys until countdown finished
-
 document.addEventListener("DOMContentLoaded", function(){
     currentBackground = bgOne;    
-
- timeHandler = setInterval(function(){
+timeHandler = setInterval(function(){
   if (seconds > 0) {
     headerText.textContent = seconds;
     seconds--;
-  } else if (lives === 0) {
-    headerText.textContent = "GAME OVER";
   } else {
     initGame();
     headerText.textContent = "TUMBLE!";
+    lvlOneMusic.play();
     setTimeout(function(){
-      headerText.textContent = "LVL 1";
+      headerText.textContent = "LEVEL 1";
     }, 3000)
     clearInterval(timeHandler);
     }
@@ -164,53 +227,135 @@ document.addEventListener("DOMContentLoaded", function(){
 // run animation function / add characters to canvas
 
 var animate = function () {
+  scoreBox.textContent = score;
+  lifeCount.textContent = lives;
+  if (lives > 0) {
   requestAnimationFrame(animate);
   ctx.clearRect(0,0, canvas.width, canvas.height);
   currentBackground.draw();
-  if(player.x >= 630 && lives > 0){
+  // LEVEL TWO STARTS
+  if(player.x >= 600){
+    levelTwo = true;
+    lvlOneMusic.stop();
+    lvlTwoMusic.play();
     clearEnemies();
     clearInterval(enemyInterval);
     enemies.forEach(function(enemy, i){
       enemy.x + 300;
       enemy.draw();
-      enemy.x -= i / 4;
+      enemy.x -= i;
   })
     enemyIntervalTwo = setInterval(function(){
-    var cobra = new Entity (player.x + 400, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/attack1.png');
-    var fox = new Entity (player.x + 500, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/002.png');
-    enemies.push(fox);
-    enemies.push(cobra);
-   }, 2500);
-    headerText.textContent = "LVL 2";
-    player.x = 0;
+    var cobra = new Entity (player.x + 500, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/attack1.png');
+    var fox = new Entity (player.x + 650, (innerWidth/2) + 20, 100, 100, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/002.png');
+    if (enemies.length < maxEnemies) {
+      enemies.push(fox);
+      enemies.push(cobra);
+  } setInterval(function() {
+      enemies.y -= 60;
+    },1000)
+    setTimeout(function() {
+      enemies.y += 60
+    }, 200);
+}, 5000);
+    enemies.length = 0;
     currentBackground = bgTwo;
+    player.x = 0;
+    headerText.textContent = "LEVEL 2";
   }
   player.draw();
   enemies.forEach(function(cowboy, i){
     cowboy.x + 300;
     cowboy.draw();
     cowboy.x -= i / 4;
-  }); if (currentBackground === bgTwo && player.x >= 580) {
+  // LEVEL THREE STARTS
+}); if (currentBackground === bgTwo && player.x >= 580) {
+      levelThree = true;
       clearEnemies();
-      clearInterval(enemyIntervalTwo);
-      enemies.forEach(function(enemy, i){
-        enemy.x + 300;
-        enemy.draw();
-      })
-      var boss = new Entity (player.x + 700, (innerWidth/2) + 100, 100, -250, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/Timonkey.png');
-      enemies.push(boss);
+      headerText.textContent = "LAST LEVEL";
       currentBackground = bgThree;
       player.x = 0;
-      headerText.textContent = "FINAL LVL";
-  }
-    if (enemies.length > 20) {
-      for (let i = 0; i < enemies.length; i++) {
-        enemies.pop();
+      lvlTwoMusic.stop();
+      lvlThreeMusic.play();
+      clearInterval(enemyIntervalTwo);
+      enemies.forEach(function(enemy, i){
+        enemy.draw();
+      });
+      var boss = new Entity (player.x + 500, (innerWidth/2) - 15, 150, 150, '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/jump-1.png');
+      enemies.push(boss);
+      enemies[0].x - 2;
+    }} if (!levelOne && !levelTwo && levelThree && enemies.length === 0) {
+      lvlThreeMusic.stop();
+      winMusic.play();
+      headerText.textContent = "YOU WIN!";
+    }
+  
+  bullets.forEach(function(bullet){
+    bullet.draw();
+    bullet.x += pace * 2;
+  });
+
+// add collision detection
+  for(let i = 0; i < enemies.length; i++) {
+    enemies[i].x -= 2;
+  if(player.x - 100 < enemies[i].x + enemies[i].width && (player.x - 100) + player.width > enemies[i].x &&
+      player.y < enemies[i].y + enemies[i].height && player.y + enemies[i].height > enemies[i].y){
+      playerHit.play();
+      player.x -= 30;
+      enemies[i].x += 300;
+      lives--;
+
+  
+} for(let i = 0; i < bullets.length; i++) {
+  if (bullets[i].x < enemies[i].x + (enemies[i].width - 30) && bullets[i].x + (enemies[i].width - 30) > enemies[i].x &&
+      bullets[i].y < enemies[i].y + enemies[i].height && bullets[i].y + enemies[i].height > enemies[i].y) {
+      score += 10;
+      bullets.splice([i],1);
+      laser.play();
+      enemies[i].src = '/Users/garrettmoore/Code/Unit1/tumblin-game/imgs/boom2.png';
+      enemies[i].x += 80;
+      enemies[i].y -= 10;
+      setTimeout(function(){
+        enemies.splice([i],1);
+      }, 500)
+    }
+}} if (lives === 0) {
+  lvlOneMusic.stop();
+  lvlTwoMusic.stop();
+  lvlThreeMusic.stop();
+  gameOverSound.play();
+  headerText.textContent = "GAME OVER";
+}};
+
+// restart function
+var restartGame = function () {
+  lvlOneMusic.stop();
+  lvlTwoMusic.stop();
+  lvlThreeMusic.stop();
+  currentBackground = bgOne;
+  seconds = 3;
+  currentBackground.x = 0;
+  lives = 3;
+  score = 0;
+  enemies.length = 0;
+  player.x = 0;
+  timeHandler = setInterval(function(){
+    if (seconds > 0) {
+      headerText.textContent = seconds;
+      seconds--;
+    } else {
+      initGame();
+      headerText.textContent = "TUMBLE!";
+      lvlOneMusic.play();
+      setTimeout(function(){
+        headerText.textContent = "LEVEL 1";
+      }, 3000)
+      clearInterval(timeHandler);
       }
-  }
-};
+    }, 1000)
+  clearInterval(enemyInterval && enemyIntervalTwo);
+  animate();
+}
 
-animate()
-
-
-
+restart.addEventListener('click', restartGame);
+animate();
